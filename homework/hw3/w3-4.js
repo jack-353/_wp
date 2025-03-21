fd8f3d3b87f03ed5de59b3529390a9e354b620c9
jack-353 <s111310535@student.nqu.edu.tw>
2025-03-21 11:55:14 +0800

    Create w3-4.js

---
+++ homework/hw3/w3-4.js
@@ -0,0 +1,12 @@
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let ans =[];
+for(let i=0;nums[i];i++)
+{
+  let x = 0;
+  let cont = 0;
+  if(nums[i]%2 == x)
+  {
+    ans.push(nums[i]);
+  }
+}
+console.log(ans);
