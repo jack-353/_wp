d516451870bd8e25627e2064cb7e9468680faf8d
jack-353 <s111310535@student.nqu.edu.tw>
2025-04-11 09:52:07 +0800

    w4-1.js

--- homework/hw4/w4-1.js
+++ homework/hw4/w4-1.js
@@ -1,1 +1,17 @@
+function countLetters(str) {
+  const letterMap = new Map();
+
+  for (let char of str) {
+    if (/[a-zA-Z]/.test(char)) { 
+      char = char.toLowerCase(); 
+      letterMap.set(char, (letterMap.get(char) || 0) + 1);
+    }
+  }
+
+  return letterMap;
+}
+
+
+console.log(countLetters("banana")); 
+
 
